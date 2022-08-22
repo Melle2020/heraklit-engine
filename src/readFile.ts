dae83ce0c0fdec55618aebc9a2ed2d4aa81c82ec
Melle2020 <mellengami@gmail.com>
2022-08-22 17:32:33 +0100

    figure 15 c generated

--- src/readFile.ts
+++ src/readFile.ts
@@ -3,9 +3,13 @@ import fs from 'fs'
 import {CliRenderer} from "@diagrams-ts/graphviz-cli-renderer";
 
 const g = digraph('G');
-const data = fs.readFileSync('./data/logicLanguage2.logic', 'utf8')
+const data = fs.readFileSync('./data/system.hera', 'utf8')
 
 const symbolTable = new Map();
+
+
+
+
 
 
 const lines = data.toString().replace(/\r\n/g,'\n').split('\n');
@@ -14,53 +18,31 @@ for (const line of lines) {
     const tokensRegExp = /([\w-]+|\(|\)|\,)/g
     const tokenList = line.match(tokensRegExp)
     // console.log("token list: " + JSON.stringify(tokenList))
+
     const openBracePos = line.indexOf('(');
     const closeBracePos = line.indexOf(')');
     const relName = line.substring(0, openBracePos);
-    console.log(relName)
+    // console.log(relName)
 
     const params = line.substring(openBracePos + 1, closeBracePos)
     const words = params.split(',')
-    // console.log("test"+words)
+
     const name = words[0].trim()
-    
-    if (relName === 'Flow') {
-         if(words.length===3){
-
-            for(let w of words){
-               let  i=0;
-                if(i=0){
-                    symbolTable.set(name, {
-                        _id: name,
-                        _type: 'Place',
-                        values: []
-                    })
-
-                }
-            }
-
-            
-            symbolTable.get(name).values.push(name)
-         }else
-         if(words.length===4){
-            symbolTable.set(name, {
-                _id: name,
-                _type: 'Place',
-                values: []
-            })
-         }
-        
-        
-        console.log("test"+ JSON.stringify(symbolTable.get(name)))
+    if (relName === 'Place') {
+        symbolTable.set(name, {
+            _id: name,
+            _type: 'Place',
+            values: []
+        })
     }
-    // else if (relName === 'Transition') {
-    //     symbolTable.set(name, {
-    //         _id: name,
-    //         _type: 'Transition',
-    //         inFlows: [],
-    //         outFlows: []
-    //     })
-    // }
+    else if (relName === 'Transition') {
+        symbolTable.set(name, {
+            _id: name,
+            _type: 'Transition',
+            inFlows: [],
+            outFlows: []
+        })
+    }
 }
 
 // second run, add flows
@@ -78,13 +60,23 @@ for (const line of lines) {
         const tgtName = words[1].trim()
         const srcObj = symbolTable.get(srcName);
         const tgtObj = symbolTable.get(tgtName);
-        console.log("value"+JSON.stringify(srcName))
 
-        // if (srcObj['_type'] === "Place") {
-        //     tgtObj["inFlows"].push(srcName);
-        // } else if (srcObj["_type"] === "Transition") {
-        //     srcObj["outFlows"].push(tgtName);
-        // }
+        if (srcObj['_type'] === "Place") {
+            if(words.length===3){
+                tgtObj["inFlows"].push(srcName+"*"+words[2]);
+            }else{
+                tgtObj["inFlows"].push(srcName+"*"+words[2]+"*"+words[3]);
+            }
+            
+            
+        } else if (srcObj["_type"] === "Transition") {
+            if(words.length===3){
+                srcObj["outFlows"].push(tgtName+"*"+words[2]);
+            }else{
+                srcObj["outFlows"].push(tgtName+"*"+words[2]+"*"+words[3]);
+            }
+           
+        }
 
     }
 
@@ -127,78 +119,63 @@ for(let elt of symbolTable.keys()){
                 [attribute.shape]: "box",
               })
     } else{
-        if(value._id.indexOf('available')!=-1){
-           const tabElt:any[]= elt.split('-');
-           tabElt.pop()
-            const node=g.createNode(elt,{[attribute.label]: tabElt.join(value.values[0])})
-            
-        }else
-        if(value._id.indexOf('client-with-item-2')!=-1){
-            const tabElt:any[]= elt.split('-');
-            tabElt.pop()
-            // console.log('values'+JSON.stringify(value.values))
-             const node=g.createNode(elt,{[attribute.label]: value.values.join(' wants a')})
-             
-         }else
-         if(value._id.indexOf('vendor-with-item-3')!=-1){
-            const tabElt:any[]= elt.split('-');
-            tabElt.pop()
-            // console.log('values'+JSON.stringify(value.values))
-             const node=g.createNode(elt,{[attribute.label]: value.values.join(' with')})
-             
-         }
-         else if(value._id.indexOf('client-with-item-4')!=-1){
-            const tabElt:any[]= elt.split('-');
-            tabElt.pop()
-            // console.log('values'+JSON.stringify(value.values))
-             const node=g.createNode(elt,{[attribute.label]: value.values.join(' with')})
-             
-         }
-        //  else{
-
-        //     const node=g.createNode(elt,{[attribute.label]: value.values.join(' with')})
-        //  }
-
-
+           
         
+        //  const node=g.createNode(elt,{[attribute.label]: " test"})
 
     }
     
 }
 
 for(let elt of symbolTable.keys()){
-    // console.log(elt);
-    // console.log("Value"+JSON.stringify(symbolTable.get(elt)))
+    console.log(elt);
+    console.log("Value"+JSON.stringify(symbolTable.get(elt)))
     const value=symbolTable.get(elt);
     if(value._type==='Transition'){
+        let i=1
         for(let item of value.inFlows){
-            const node=g.createEdge([item,elt])
-        }
-
-        for(let item of value.outFlows){
-            const node=g.createEdge([elt,item])
             
+            const words = item.split('*')
+             if(words.length===2){
+                const node=g.createEdge([""+i+"",elt],{
+                    [attribute.label]: words[1]
+    
+                })
+             }
+             else{
+                const node=g.createEdge([""+i+"",elt],{
+                    [attribute.label]:"(" +words[1] +", "+ words[2]+ ")"
+    
+                })
+             }
+             i++;
+            
+        }
+      
+        for(let item of value.outFlows){
+            const words = item.split('*')
+            // const node=g.createEdge([elt,item])
+            
+            if(words.length===2){
+                
+                const node=g.createEdge([elt,""+i+""],{
+                    [attribute.label]: words[1]
+    
+                })
+             }
+             else{
+                const node=g.createEdge([elt,""+i+""],{
+                    [attribute.label]:"(" +words[1] +" ,"+ words[2]+ ")"
+    
+                })
+             }
+             i++;
         }
         
     } 
 }
 
 
-
-// const nodeA1 = subgraphA.createNode('A_node1',{
-//     [attribute.shape]: "box",
-//   });
-// const nodeA2 = subgraphA.createNode('A_node2');
-// subgraphA.createEdge([nodeA1, nodeA2]);
-
-// const subgraphB = g.createSubgraph('B');
-// const nodeB1 = subgraphB.createNode('B_node1');
-// const nodeB2 = subgraphB.createNode('B_node2');
-// subgraphA.createEdge([nodeB1, nodeB2]);
-
-// const node1 = g.createNode('node1');
-// const node2 = g.createNode('node2');
-// g.createEdge([node1, node2]);
 const dot = toDot(g);
 console.log(dot);
 
